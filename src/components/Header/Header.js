1da190698f79fc03f334aaedfaaaabc160ddd241
marianadeoliveiraosf <mariana.deoliveira@osf.digital>
2022-01-07 07:31:32 -0300

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,24 @@
+import * as S from "./Header.style";
 import React from "react";
+
+import { Link } from "@ds/react";
+
 import { ReactComponent as CNPlogo } from "assets/icons/Logo-CNP.svg";
 import { ReactComponent as ChatIcon } from "assets/icons/chatblue.svg";
 import { ReactComponent as AccessibilityIcon } from "assets/icons/signs-languageblue.svg";
-import * as S from "./Header.style";
-
 const Header = () => {
   return (
     <>
       <S.Header>
-        <CNPlogo />
         <S.Container>
-          <ChatIcon className="chatIcon" />
-          <AccessibilityIcon />
+          <Link variant="primary-md-02" className="Link">
+            &#8249; Voltar
+          </Link>
+          <CNPlogo />
+          <S.ContainerImg>
+            <ChatIcon className="chatIcon" />
+            <AccessibilityIcon />
+          </S.ContainerImg>
         </S.Container>
       </S.Header>
       <S.Gap></S.Gap>
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -2,23 +2,37 @@ import styled from "styled-components/macro";
 
 export const Header = styled.div(({ theme: { breakpoint, spacing } }) => ({
   width: "100%",
-  display: "flex",
   padding: "16px 50px",
   maxWidth: "1440px",
   margin: "auto",
 
+  " .Link span": {
+    fontSize: "16px",
+    textDecoration: "none",
+  },
+
   [breakpoint.lg()]: {
     padding: "16px 123px",
+    " .Link span": {
+      display: "none",
+    },
   },
 }));
-export const Container = styled.div(() => ({
-  display: "flex",
-  justifyContent: "flex-end",
+export const Container = styled.div(({ theme: { breakpoint } }) => ({
   width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+
+  [breakpoint.lg()]: {},
+}));
+
+export const ContainerImg = styled.div(({ theme: { breakpoint } }) => ({
   marginTop: "16px",
   ".chatIcon": {
     marginRight: "20px",
   },
+
+  [breakpoint.lg()]: {},
 }));
 
 export const Gap = styled.div(({ theme: { color } }) => ({
